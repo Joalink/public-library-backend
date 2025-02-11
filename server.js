9e8fce633616f1206aaa6c409eae0f86683f7d10
JoaquinDS <joaquin.hz.p@outlook.com>
2025-02-11 15:02:34 -0600

    add cors error, allow origin

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 require("dotenv").config();
 require("module-alias/register");
 const express = require("express");
-const booksRouter = require("./routes/api/bookRoutes");
-// const booksRouter = require('./routes/api/books');
-
-
-// const morgan = require('morgan');
+const cors = require("cors");
 const app = express();
+const booksRouter = require("./routes/api/bookRoutes");
+
+// const booksRouter = require('./routes/api/books');
+// const morgan = require('morgan');
+
+app.use(
+  cors({
+    origin: "http://localhost:8081",
+    // methods: ["GET", "POST", "PUT", "DELETE"]
+    allowedHeaders: ["Content-Type", "Authorization"], 
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/v1/books", booksRouter);
